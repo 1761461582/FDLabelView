9b08d2cc11300d1ef6d78f77294e655721f58158
magic <magic@fourdesire.com>
2013-08-08 14:11:13 +0800

    Initial Commit

---
+++ FDLabelView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FDLabelView
+//
+//  Created by magic on 8/8/13.
+//  Copyright (c) 2013 Fourdesire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FDLabelView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FDLabelView
+//
+//  Created by magic on 8/8/13.
+//  Copyright (c) 2013 Fourdesire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
